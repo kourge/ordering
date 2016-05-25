88648462d56f4d5cf9184ad48e5da28ee34c0b6e
Wil Lee <kourge@gmail.com>
2016-05-25 01:26:13 -0700

    Quote identifiers properly in inline docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import assign = require('object-assign');
 
 /**
- * An Ordering<T> can compare two items of type T. A return value of 0 means
+ * An `Ordering<T>` can compare two items of type `T`. A return value of 0 means
  * the two items are equal, a value lesser than 0 means `a` is lesser than `b`,
  * and a value greater than 0 means `a` is great than `b`.
  */
@@ -9,36 +9,9 @@ export interface Ordering<T> {
   (a: T, b: T): number;
 }
 
-/**
- * A WrappedOrdering<T> provides convenience methods for using an existing
- * ordering or deriving another ordering out of an existing one.
- */
-export class WrappedOrdering<T> {
-  constructor(public ordering: Ordering<T>) {}
-
-  /**
-   * Returns an ordering that is a reversal of the current one.
-   */
-  reverse(): WrappedOrdering<T> {
-    return new WrappedOrdering((a: T, b: T): number =>
-      -this.ordering(a, b)
-    );
-  }
-
-  /**
-   * Derives an Ordering<U> out of the current Ordering<T> given a
-   * transformation function from U to T.
-   */
-  on<U>(f: (data: U) => T): WrappedOrdering<U> {
-    return new WrappedOrdering((a: U, b: U): number =>
-      this.ordering(f(a), f(b))
-    );
-  }
-}
-
 export namespace Ordering {
   /**
-   * Wraps a plain Ordering<T> in a WrappedOrdering<T>.
+   * Wraps a plain `Ordering<T>` in a `WrappedOrdering<T>`.
    */
   export function of<T>(ordering: Ordering<T>): WrappedOrdering<T> {
     return new WrappedOrdering(ordering);
@@ -71,6 +44,33 @@ export namespace Ordering {
           return last(a, b);
         };
     }
+  }
+}
+
+/**
+ * A `WrappedOrdering<T>` provides convenience methods for using an existing
+ * ordering or deriving another ordering out of an existing one.
+ */
+export class WrappedOrdering<T> {
+  constructor(public ordering: Ordering<T>) {}
+
+  /**
+   * Returns an ordering that is a reversal of the current one.
+   */
+  reverse(): WrappedOrdering<T> {
+    return new WrappedOrdering((a: T, b: T): number =>
+      -this.ordering(a, b)
+    );
+  }
+
+  /**
+   * Derives an `Ordering<U>` out of the current `Ordering<T>` given a
+   * transformation function from `U` to `T`.
+   */
+  on<U>(f: (data: U) => T): WrappedOrdering<U> {
+    return new WrappedOrdering((a: U, b: U): number =>
+      this.ordering(f(a), f(b))
+    );
   }
 }
 
