bc68d6e1d0d3ade33728409861943274014c5efa
Wil Lee <kourge@gmail.com>
2020-04-10 19:54:14 -0700

    Simplify JSDoc wording

--- comparators.ts
+++ comparators.ts
@@ -1,13 +1,13 @@
 /**
- * A comparator that compares numbers.
+ * Compares numbers.
  */
 export function byNumber(a: number, b: number): number {
   return a - b;
 }
 
 /**
- * A comparator that compares strings lexicographically according to the
- * current locale's collation.
+ * Compares strings lexicographically according to the current locale's
+ * collation.
  */
 export function byString(a: string, b: string): number {
   return a.localeCompare(b);
@@ -15,9 +15,9 @@ export function byString(a: string, b: string): number {
 
 export namespace byString {
   /**
-   * A comparator that compares strings lexicographically according to the
-   * current locale's collation, but does so while ignoring case as defined by
-   * the current locale.
+   * Compares strings lexicographically according to the current locale's
+   * collation, but does so while ignoring case as defined by the current
+   * locale.
    */
   export function caseInsensitive(a: string, b: string): number {
     return a.localeCompare(b, 'en-US', {sensitivity: 'base'});
@@ -25,16 +25,15 @@ export namespace byString {
 }
 
 /**
- * A comparator that compares dates by chronological order. An earlier date is
- * considered smaller than a later one.
+ * Compares dates by chronological order. An earlier date is considered smaller
+ * than a later one.
  */
 export function byDate(a: Date, b: Date): number {
   return +a - +b;
 }
 
 /**
- * A comparator that compares booleans by truth. Falsehood is considered
- * smaller than truth.
+ * Compares booleans by truthiness. Falsehood is considered smaller than truth.
  */
 export function byBoolean(a: boolean, b: boolean): number {
   return +a - +b;
